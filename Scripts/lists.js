b85a8107499576548583c444a328e82357b712ff
Mavis Davis <mavisledavis563@gmail.com>
2026-04-18 17:30:07 -0500

    Fix typo in textList description

--- Scripts/lists.js
+++ Scripts/lists.js
@@ -1,6 +1,6 @@
 var textList = ["Nettle is a goblin, short statured even for one of her kind, if I were to make a guess, I would say taller than a pumpkin, though maybe not taller than a pumpkin with a couple of cabbages stacked on top. Though her height matches her temper, I guess the universe evens things out in its own way.", 
-            "Nettle lives just off of the beaten bath, on the outskirts of the bustling town of Dirtfarm (really creative name, I know). Though it's quite peaceful, being so removed from everything. One of the rare places where you can just, forget the world. Get lost in a good book.",  
 			"", 
+            "Nettle lives just off of the beaten bath, on the outskirts of the bustling town of Dirtfarm (really creative name, I know). Though it's quite peaceful, being so removed from everything. One of the rare places where you can just, forget the world. Get lost in a good book.",  
             "I imagine Nettle has a lot of time to think about nothing in particular.", 
             "Nettle lives in a small, dusty house with her parents. On the outskirts of a bustling town. It's peaceful in a way, being so far from everything.", 
             "In the middle of said town is a school, an institution of learning. Nettle goes there every day. Not by her own choice of course, for her parents tell her to go (and it's also kinda the law or whatever). Not to imply that she dislikes school, in fact, one could say she quite enjoys \"learning\".", 
