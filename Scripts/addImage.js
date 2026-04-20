b824f8890df9b1ff404ea658b4b02533e5292f38
Mavis Davis <mavisledavis563@gmail.com>
2026-04-20 18:23:21 -0500

    Add background color changes based on textIndex

--- Scripts/addImage.js
+++ Scripts/addImage.js
@@ -9,11 +9,33 @@ function imageChange() {
 }  
 
 function transCheck() {   
-    if (textIndex == 7) { 
+    if (textIndex == 7) {  
+        //music comes in here
         $("#text-container").css("background-color", "blue");
         $("#text-box").css("background-color", "blue");   
         $(".ext").css("background-color", "blue");  
         $("#view").css("background-color", "white");  
+        $("#text").css("color", "white"); 
+    } 
+    if (textIndex == 39) { 
+        $("#text-container").css("background-color", "grey");
+        $("#text-box").css("background-color", "black");   
+        $(".ext").css("background-color", "grey");  
+        $("#view").css("background-color", "light-prey");  
+        $("#text").css("color", "white"); 
+    } 
+    if (textIndex == 58) { 
+        $("#text-container").css("background-color", "green");
+        $("#text-box").css("background-color", "green");   
+        $(".ext").css("background-color", "green");  
+        $("#view").css("background-color", "white");  
+        $("#text").css("color", "white"); 
+    } 
+    if (textIndex == 108) { 
+        $("#text-container").css("background-color", "black");
+        $("#text-box").css("background-color", "black");   
+        $(".ext").css("background-color", "black");  
+        $("#view").css("background-color", "black");  
         $("#text").css("color", "white"); 
     }
 
